33233e6ffb8da93bacf0891137f69167800d8991
Kihyun <kihyun.math.ee@gmail.com>
2026-01-26 13:54:46 +0900

    Refactor: BOJ 14681 optimize with sys.stdin and nested if

--- 14681.py
+++ 14681.py
@@ -1,11 +1,11 @@
-x = int(input())
-y = int(input())
+import sys
 
-if x > 0 and y > 0:
-    print('1')
-elif x < 0 and y > 0:
-    print('2')
-elif x < 0 and y < 0:
-    print('3')
+# 속도 최적화를 위해 sys.stdin.readline 사용
+x = int(sys.stdin.readline())
+y = int(sys.stdin.readline())
+
+# 중첩 조건문으로 비교 횟수 최적화 (Nested If)
+if x > 0:
+    print(1 if y > 0 else 4)
 else:
-    print('4')
+    print(2 if y > 0 else 3)
