f3d874e2318cd8ddcf09169b2828c173dd8b5425
Kihyun <kihyun.math.ee@gmail.com>
2026-01-31 18:53:19 +0900

    Refactor: BOJ 10807 optimize with list.count()

--- 10807.py
+++ 10807.py
@@ -3,8 +3,4 @@ N = int(sys.stdin.readline())
 data = list(map(int, sys.stdin.readline().split()))
 v = int(sys.stdin.readline())
 
-cnt = 0
-for x in data:
-    if x == v:
-        cnt += 1
-print(cnt)
+print(data.count(v))
