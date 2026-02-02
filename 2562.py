5ae164572e737ff36720f81e20cc9b861653b8b0
Kihyun <kihyun.math.ee@gmail.com>
2026-02-02 12:13:12 +0900

    Add: BOJ 2562 Find Max and Index

---
+++ 2562.py
@@ -0,0 +1,10 @@
+import sys
+
+num_list = []
+for _ in range(9):
+    N = int(sys.stdin.readline())
+    num_list.append(N)
+    max_val = max(num_list)
+
+print(max_val)
+print(num_list.index(max_val) + 1)
