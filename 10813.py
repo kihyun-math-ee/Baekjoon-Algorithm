e7687d2d8979f0bbe7db3016db41f87fb51b2187
Kihyun <kihyun.math.ee@gmail.com>
2026-02-03 11:05:12 +0900

    Add: BOJ 10813 ball_exchange (Pythonic Swap)

---
+++ 10813.py
@@ -0,0 +1,9 @@
+import sys
+N, M = map(int, sys.stdin.readline().split())
+basket = list(range(1, N + 1))
+
+for _ in range(M):
+    i, j = map(int, sys.stdin.readline().split())
+    basket[i-1], basket[j-1] = basket[j-1], basket[i-1]
+
+print(*basket)
