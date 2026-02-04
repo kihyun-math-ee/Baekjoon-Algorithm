9b10cc1fa1028ad6194cf2ec137d9e547f51c3bf
Kihyun <kihyun.math.ee@gmail.com>
2026-02-04 15:24:57 +0900

    Refactor: BOJ 1546 optimize calculation (Math Formula)

--- 1546.py
+++ 1546.py
@@ -1,10 +1,6 @@
 import sys
+
 N = int(sys.stdin.readline())
 scores = list(map(int, sys.stdin.readline().split()))
-M = max(scores)
 
-new_scores = []
-for s in scores:
-    new_scores.append(s / M * 100)
-    
-print(sum(new_scores) / N)
+print(sum(scores) * 100 / max(scores) / N)
