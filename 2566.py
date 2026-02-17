4c353bef180b6eabfa2104f02e9a556652ea5f2e
Kihyun <kihyun.math.ee@gmail.com>
2026-02-17 23:03:39 +0900

    Refactor: BOJ 2566 Encapsulate grid logic in Matrix class

--- 2566.py
+++ 2566.py
@@ -1,20 +1,30 @@
 import sys
+class Matrix:
+    def __init__(self):
+        self.matrix = []
 
-A = []
-max_val = -1
-row_idx = 0
-col_idx = 0
+    def add_row(self, row):
+        self.matrix.append(row)
 
-for i in range(9):
-    row = list(map(int, sys.stdin.readline().split()))
-    A.append(row)
+    def find_max(self):
+        maximum = -1
+        max_row = 0
+        max_col = 0
+
+        for i in range(9):
+            for j in range(9):
+                if self.matrix[i][j] > maximum:
+                    maximum = self.matrix[i][j]
+                    max_row = i + 1
+                    max_col = j + 1
+        print(maximum)
+        print(max_row, max_col)
+
+if __name__ == "__main__":
+    my_matrix = Matrix()
     
-for i in range(9):
-    for j in range(9):
-        if A[i][j] > max_val:
-            max_val = A[i][j]
-            row_idx = i + 1
-            col_idx = j + 1
+    for i in range(9):
+        row = list(map(int, sys.stdin.readline().split()))
+        my_matrix.add_row(row)
 
-print(max_val)
-print(row_idx, col_idx)
+    my_matrix.find_max()
