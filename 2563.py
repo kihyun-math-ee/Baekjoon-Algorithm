544b9a1eea5dc9e56061920462acd4033599d908
Kihyun <kihyun.math.ee@gmail.com>
2026-02-18 16:41:38 +0900

    Refactor: BOJ 2563 Color Paper - Implemented 2D Boolean Array strategy in class

--- 2563.py
+++ 2563.py
@@ -1,17 +1,28 @@
 import sys
+class Matrix:
+    def __init__(self):
+        self.matrix =[[0] * 101 for _ in range(101)] # for evading index error
+        self.area = 0
 
-paper = [[0] * 101 for _ in range(101)]
-total_area = 0
+    def pixel(self, a, b):
 
-N = int(sys.stdin.readline())
+        for i in range(a, a + 10):
+            for j in range(b, b + 10):
+                if self.matrix[i][j] == 0:
+                    self.matrix[i][j] = 1
+                    self.area += 1
 
-for _ in range(N):
-    x, y = map(int, sys.stdin.readline().split())
+
+    def result(self):
+        print(self.area)
     
-    for i in range(x, x + 10):
-        for j in range(y, y + 10):
-            if paper[i][j] == 0:
-                paper[i][j] = 1
-                total_area += 1
+if __name__ == "__main__":
+    N = int(sys.stdin.readline())
+    my_matrix = Matrix()
+    for _ in range(N):
+        a, b = map(int, sys.stdin.readline().split())
+        my_matrix.pixel(a, b)
 
-print(total_area)
+    my_matrix.result()
+
+        
