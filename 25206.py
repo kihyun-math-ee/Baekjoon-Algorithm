3c0db9f1cbfa122ca075c5d37e4d4a61af9f4ad1
Kihyun <kihyun.math.ee@gmail.com>
2026-02-19 12:10:38 +0900

    Refactor: BOJ 25206 Your Major - Implemented GPA Calculator Class

--- 25206.py
+++ 25206.py
@@ -1,22 +1,33 @@
 import sys
 
-grade_dict = {
-    "A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, 
-    "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0
-}
+class GPA:
+    def __init__(self):
+        self.dict_gpa = {"A+": 4.5, "A0": 4.0, "B+": 3.5, "B0": 3.0, 
+                         "C+": 2.5, "C0": 2.0, "D+": 1.5, "D0": 1.0, "F": 0.0}
 
-total_score = 0.0
-total_credit = 0.0
+    def get_score(self, credit, grade):
+        return credit * self.dict_gpa[grade]
 
-for _ in range(20):
-    line = sys.stdin.readline().split()
-    credit = float(line[1])
-    grade_char = line[2]
-
-    if grade_char == 'P':
-        continue
+if __name__ == "__main__":
+    my_calculator = GPA() 
     
-    total_score += credit * grade_dict[grade_char]
-    total_credit += credit
-
-print(total_score / total_credit)
+    total_credits = 0.0
+    total_score = 0.0
+    
+    for _ in range(20):
+        line = sys.stdin.readline().split()
+        if not line: break
+        
+        credits = float(line[1])
+        grade = line[2]
+        
+        if grade == 'P':
+            continue
+        
+        total_credits += credits
+        total_score += my_calculator.get_score(credits, grade)
+    
+    if total_credits == 0:
+        print(0.0)
+    else:
+        print(total_score / total_credits)
