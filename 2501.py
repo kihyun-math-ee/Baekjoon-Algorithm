24745128b96ae1342023611d7984f0ae356e703f
Kihyun <kihyun.math.ee@gmail.com>
2026-02-19 13:44:26 +0900

    Add: BOJ 2501 Divisors - Finding K-th Smallest Divisor

---
+++ 2501.py
@@ -0,0 +1,12 @@
+import sys
+N, K = map(int, sys.stdin.readline().split())
+count = 0
+for i in range(1, N + 1):
+    if N % i == 0:
+        count += 1
+        if count == K:
+            print(i)
+            break
+if count < K:
+    print(0)
+        
