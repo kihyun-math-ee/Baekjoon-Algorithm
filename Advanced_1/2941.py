25df8e77c3833932c89e9b8785ae71a06d6c6a3d
Kihyun <kihyun.math.ee@gmail.com>
2026-02-20 12:09:35 +0900

    Refactor: BOJ 2941 Croatian Alphabet - Refactored string replacement logic using OOP

--- Advanced_1/2941.py
+++ Advanced_1/2941.py
@@ -1,8 +1,16 @@
 import sys
-word = sys.stdin.readline().strip()
-croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
 
-for i in croatia:
-    word = word.replace(i, '*')
+class Croatian:
+    def __init__(self):
+        self.croatian = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+        
+    def Replace(self, alphabet):
+        for i in range(len(self.croatian)):
+            alphabet = alphabet.replace(self.croatian[i], 'X')
+        print(len(alphabet))
 
-print(len(word))
+    
+if __name__ == "__main__":
+    my_croatian = Croatian()
+    S = sys.stdin.readline().strip()
+    my_croatian.Replace(S)
