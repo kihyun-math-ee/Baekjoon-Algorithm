d3ba3da061c491377db6c84240a438545a0bfecd
Kihyun <kihyun.math.ee@gmail.com>
2026-02-21 20:40:19 +0900

    Refactor: Remove beginner syntax annotations to enforce clean code and self-documenting logic

--- Conditionals/1330.py
+++ Conditionals/1330.py
@@ -1,15 +1,16 @@
-A, B = map(int, input().split()) # 1단계: 문자열 입력 -> 쪼개기 -> 정수로 변환(매핑) 
+A, B = map(int, input().split()) 
 
   
 
-if A > B:          # 중요: 조건문 끝에는 반드시 콜론(:) 필수! 
+if A > B:          
 
-    print(">")     # 큰따옴표("")는 문자열 그 자체를 출력 
+    print(">")
 
-elif A < B:        # Python에서는 else if 대신 'elif'를 사용 
+elif A < B:       
 
     print("<") 
 
-else:              # 중요: else 뒤에도 콜론(:) 필수! 
+else:            
+
 
     print("==") 
