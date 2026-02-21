80462f2639a8d9cae744363ca372ae335017be98
Kihyun <kihyun.math.ee@gmail.com>
2026-02-21 20:41:36 +0900

    Refactor: Remove beginner syntax annotations to enforce clean code and self-documenting logic

--- Conditionals/2480.py
+++ Conditionals/2480.py
@@ -2,15 +2,15 @@ import sys
 
 A, B, C = map(int, sys.stdin.readline().split())
 
-# 1. 3개 모두 같은 경우
 if A == B == C:
     print(10000 + A * 1000)
-# 2. 2개만 같은 경우 (A가 B 또는 C와 같으면 겹치는 눈은 A)
+
 elif A == B or A == C:
     print(1000 + A * 100)
-# 2-1. 남은 경우 중 B와 C가 같은 경우
+
 elif B == C:
     print(1000 + B * 100)
-# 3. 모두 다른 경우 (max 함수 활용)
+
 else:
     print(max(A, B, C) * 100)
+
