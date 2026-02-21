d368250b7db6c66b398d09eab029cc3fbb08ca8c
Kihyun <kihyun.math.ee@gmail.com>
2026-02-21 16:53:12 +0900

    Refactor: BOJ 1157 Word Study - Refactored using OOP architecture and optimized ASCII index mapping

--- Advanced_1/1157.py
+++ Advanced_1/1157.py
@@ -1,17 +1,35 @@
 import sys
-word = sys.stdin.readline().strip().upper()
-unique_chars = list(set(word))
-cnt_list = []
 
-for x in unique_chars:
-    cnt = word.count(x)
-    cnt_list.append(cnt)
-    
-if cnt_list.count(max(cnt_list)) > 1:
-    print("?")
-else:
-    max_idx = cnt_list.index(max(cnt_list))
-    print(unique_chars[max_idx])
+class Most:
+    def __init__(self):
+        self.alphabet = [0] * 26
+
+    def Count(self, letters):
+        self.modified_letters = letters.upper()
+        
+        for i in range(len(self.modified_letters)):
+            self.alphabet[ord(self.modified_letters[i]) - ord("A")] += 1
+
+        self.max_count = max(self.alphabet)
+        self.duplication_count = 0
+
+        for j in range(26):
+            if self.alphabet[j] == self.max_count:
+                self.duplication_count += 1
+        
+
+        if self.duplication_count > 1:
+            print("?")
+
+        else:
+            winner_index = self.alphabet.index(self.max_count)
+            print(chr(winner_index + ord('A')))
+
+if __name__ == "__main__":
+    my_most = Most()
+    S = sys.stdin.readline().strip()
+    my_most.Count(S)
+
      
 
 
