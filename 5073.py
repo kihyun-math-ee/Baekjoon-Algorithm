f6846ef6dedde755a4e071321ea7e3c1c12ea172
Kihyun <kihyun.math.ee@gmail.com>
2026-02-21 17:06:14 +0900

    Add: BOJ 5073 Triangles and Three Sides - Validated Triangle Inequality Theorem using strict boolean logic

---
+++ 5073.py
@@ -0,0 +1,20 @@
+import sys
+
+while True:
+    
+    a, b, c = map(int, sys.stdin.readline().split())
+    
+    if a == b == c == 0:
+        break
+
+    elif a == b == c:
+        print("Equilateral")
+    
+    elif (a == b or b == c or c == a) and (a + b > c and b + c > a and c + a > b):
+        print("Isosceles")
+
+    elif (a + b > c and b + c > a and c + a > b): # it guarantees there are no same length lines by first elif
+        print("Scalene")
+
+    else:
+        print("Invalid")
