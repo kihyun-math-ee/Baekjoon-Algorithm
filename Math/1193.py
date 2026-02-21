5bc272d6df9c10e0a5f6bec41f040dd737358b33
Kihyun <kihyun.math.ee@gmail.com>
2026-02-21 20:48:35 +0900

    Refactor: Remove beginner syntax annotations to enforce clean code and self-documenting logic

--- Math/1193.py
+++ Math/1193.py
@@ -1,8 +1,7 @@
 import sys
 X = int(sys.stdin.readline())
 N = 0
-# [{(N-1)(N)}/2] + 1 <= X <= {N(N+1)}/2
-# if N % 2 = 0, i / 2N - i | if N % 2 = 1, (2N-1) - i / i
+
 while True:
     try:
         N += 1
@@ -19,3 +18,4 @@ while True:
                     if X - ((N-1)*(N)//2) + 1 == j:
                         print(f'{((2*N-1)-j)}/{j+1}')
         break
+
