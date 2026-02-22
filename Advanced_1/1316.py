662e8f97ef156127b3b0db5dfc93ae69475cfcc8
Kihyun <kihyun.math.ee@gmail.com>
2026-02-22 17:49:09 +0900

    Refactor: BOJ 1316 Group Word Checker (OOP State Management & String Processing)

--- Advanced_1/1316.py
+++ Advanced_1/1316.py
@@ -1,11 +1,34 @@
 import sys
-N = int(sys.stdin.readline())
-cnt = 0
 
-for _ in range(N):
-    word = sys.stdin.readline().strip()
-    indices = [word.find(x) for x in word]
-    if indices == sorted(indices):
-        cnt += 1
+class Group_voca:
+    
+    def __init__(self):
+        self.cnt = 0
+
+    def is_group_check(self, X):
         
-print(cnt)
+        self.group_voca_check = True
+        self.group_list = [0] * 127
+
+        for i in range(len(X)):
+            if self.group_list[ord(X[i])] == 0:
+                self.group_list[ord(X[i])] = 1
+            elif self.group_list[ord(X[i])] == 1:
+                if X[i] != X[i-1]:
+                    self.group_voca_check = False
+                    break
+        if self.group_voca_check == True:
+            self.cnt += 1
+            
+    def result(self):
+        print(self.cnt)
+        
+                
+if __name__ == "__main__":
+    N = int(sys.stdin.readline())
+    my_group_voca = Group_voca()
+    for _ in range(N):
+        word = sys.stdin.readline().strip()
+        my_group_voca.is_group_check(word)
+
+    my_group_voca.result()
