7ab813e8ad8d9bd78fa7f21e533f2b2fb9bf651c
Kihyun <kihyun.math.ee@gmail.com>
2026-02-23 15:14:31 +0900

    Refactor: BOJ 1152 Word Count (OOP Architecture & Pythonic Empty String Handling)

--- Strings/1152.py
+++ Strings/1152.py
@@ -1,4 +1,15 @@
 import sys
 
-words = sys.stdin.readline().split()
-print(len(words))
+class WordCounter:
+    def __init__(self):
+        self.cnt = 1
+
+    def Blank_Count(self, X):
+    
+        words = X.split()
+        print(len(words))
+
+if __name__ == "__main__":
+    my_wordcounter = WordCounter()
+    S = sys.stdin.readline().strip()
+    my_wordcounter.Blank_Count(S)
