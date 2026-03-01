b6728edc0624702bf6c93d274fe65b97824df78c
Kihyun <kihyun.math.ee@gmail.com>
2026-03-01 15:57:48 +0900

    Refactor: BOJ 2675 string repetition logic into an OOP class structure

--- Strings/2675.py
+++ Strings/2675.py
@@ -1,11 +1,20 @@
 import sys
-T = int(sys.stdin.readline())
 
-for _ in range(T):
-    data = sys.stdin.readline().split()
-    R = int(data[0])
-    S = data[1]
+class Word_duplicate:
+    def __init__(self):
+        None
 
-    for x in S:
-        print(x * R, end='') 
-    print()
+    def word_print(self, X, number):
+        for char in X:
+            print((char * number), end='')
+        
+        print()
+
+if __name__ == "__main__":
+    my_word_duplicate = Word_duplicate()
+    execution_num = int(sys.stdin.readline())
+    for _ in range(execution_num):
+        execution_set = sys.stdin.readline().split()
+        duplicate_num = int(execution_set[0])
+        duplicate_word = execution_set[1]
+        my_word_duplicate.word_print(duplicate_word, duplicate_num)
