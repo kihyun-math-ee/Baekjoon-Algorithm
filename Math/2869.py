b42fdd075c85b4455bd055d7292ec32b70f0695a
Kihyun <kihyun.math.ee@gmail.com>
2026-03-03 12:50:14 +0900

    Refactor: BOJ 2869 (O(1) Math simulation wrapped in OOP Class)

--- Math/2869.py
+++ Math/2869.py
@@ -1,8 +1,16 @@
 import sys
 import math
-A, B, V = map(int, sys.stdin.readline().split())
-T_initial = ((V-A)/(A-B))
-ceiled_result = math.ceil(T_initial)
-print(ceiled_result + 1)
 
+class Snail_climb:
+    def __init__(self):
+        None
+
+    def climb_formula(self, A, B, V):
+        self.result = math.ceil((V - A) / (A - B)) + 1
+        print(self.result)
+
+if __name__ == "__main__":
+    my_snail_climb = Snail_climb()
+    climb, slip, height = map(int, sys.stdin.readline().split())
+    my_snail_climb.climb_formula(climb, slip, height)
 
