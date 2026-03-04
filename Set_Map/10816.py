da8fec1eeb77820c32b471c02b718b2b2e3c9723
Kihyun <kihyun.math.ee@gmail.com>
2026-03-04 18:15:29 +0900

    Add: BOJ 10816 (O(1) Hash Map Architecture using Dictionary to bypass O(N*M) TLE)

---
+++ Set_Map/10816.py
@@ -0,0 +1,22 @@
+import sys
+
+N = int(sys.stdin.readline())
+my_cards = list(map(int, sys.stdin.readline().split()))
+M = int(sys.stdin.readline())
+target_cards = list(map(int, sys.stdin.readline().split()))
+check_cards = {val: idx for idx, val in enumerate(target_cards)}
+cnt_cards = {}
+
+for x in my_cards:
+    if x in cnt_cards:
+        cnt_cards[x] += 1
+    else:
+        cnt_cards[x] = 1
+
+answer = []
+for y in target_cards:
+    answer.append(cnt_cards.get(y, 0))
+
+print(*answer)
+
+
