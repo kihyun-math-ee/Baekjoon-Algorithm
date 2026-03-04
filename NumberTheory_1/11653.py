e37a021f9c01220fed49d3bc3c8aba38797f6999
Kihyun <kihyun.math.ee@gmail.com>
2026-03-04 12:58:16 +0900

    Refactor: BOJ 11653 (Prime Factorization using Dynamic Loop and OOP Architecture)

--- NumberTheory_1/11653.py
+++ NumberTheory_1/11653.py
@@ -1,16 +1,23 @@
 import sys
-N = int(sys.stdin.readline())
-factors = []
 
-if N == 1:
-    print()
+class Factorization:
+    def __init__(self):
+        pass
 
-else:
-    i = 2
-    while N > 1:
-        while N % i == 0:
-            factors.append(i)
-            N = N // i
-        i += 1
+    def Factorize_num(self, number):
+        if number == 1:
+            return 
+            
+        i = 2
+        
+        while 1 < number:
+            while number % i == 0:
+                print(i) 
+                number = number // i
+            i += 1
 
-    print(*factors, sep="\n")
+if __name__ == "__main__":
+    my_factorization = Factorization()
+    N = int(sys.stdin.readline())
+    my_factorization.Factorize_num(N)
+
