e0bc2ea79b6b69b08e448e8030e4ec88e8b47f88
Kihyun <kihyun.math.ee@gmail.com>
2026-03-05 20:08:34 +0900

    Refactor: BOJ 2581 Prime Numbers (Prime Checking & Boundary Conditions)

--- NumberTheory_1/2581.py
+++ NumberTheory_1/2581.py
@@ -1,23 +1,28 @@
 import sys
 
-M = int(sys.stdin.readline())
-N = int(sys.stdin.readline())
-S = []
+class Find_Prime:
+    def __init__(self):
+        self.prime_set = set()
 
-for i in range(M, N + 1):
-    if i == 1:
-        continue
-    is_prime = True
+    def boundary(self, leftend, rightend):
+        for i in range(leftend, rightend + 1):
+            if i < 2:
+                continue
+                
+            for j in range(2, i):
+                if i % j == 0:
+                    break
+            else:
+                self.prime_set.add(i)
 
-    for j in range(2, i):
-        if i % j == 0:
-            is_prime = False
-            break
-    if is_prime == True:
-        S.append(i)
+        if not self.prime_set:
+            print(-1)
+        else:
+            print(sum(self.prime_set))
+            print(min(self.prime_set))
 
-if len(S) == 0:
-    print(-1)
-else:
-    print(sum(S))
-    print(min(S))
+if __name__ == "__main__":
+    my_find_prime = Find_Prime()
+    M = int(sys.stdin.readline())
+    N = int(sys.stdin.readline())
+    my_find_prime.boundary(M, N)
