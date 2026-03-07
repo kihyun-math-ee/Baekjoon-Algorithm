4d2b05bd8c4ae9f079fadd306b543033357db9c7
Kihyun <kihyun.math.ee@gmail.com>
2026-03-07 17:38:33 +0900

    Refactor: BOJ 2485 optimize interval math to O(1)

--- 2485.py
+++ 2485.py
@@ -4,7 +4,6 @@ import math
 N = int(sys.stdin.readline())
 list_coordinates = []
 list_coordinates_distance = []
-cnt = 0
 
 for _ in range(N):
     coordinate = int(sys.stdin.readline())
@@ -14,10 +13,7 @@ for i in range(N - 1):
     list_coordinates_distance.append(list_coordinates[i + 1] - list_coordinates[i])
 
 min_coordinate_distance = math.gcd(*list_coordinates_distance)
-max_trees = (list_coordinates[-1] - list_coordinates[0]) // min_coordinate_distance
+max_trees_gaps = (list_coordinates[-1] - list_coordinates[0]) // min_coordinate_distance
+plant_trees = (max_trees_gaps + 1) - len(list_coordinates)
 
-for j in range(max_trees):
-    if list_coordinates[0] + (j * min_coordinate_distance) not in list_coordinates:
-        cnt += 1
-
-print(cnt)
+print(plant_trees)
