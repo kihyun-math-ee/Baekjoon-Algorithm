fd5ef39e78cc40f8d8dd58dd986f0a3f67f14e04
Kihyun <kihyun.math.ee@gmail.com>
2026-03-14 12:58:41 +0900

    Refactor: BOJ 18870 Coordinate Compression (OOP, Set Deduplication & Dictionary Enumerate Mapping)

--- Sorting/18870.py
+++ Sorting/18870.py
@@ -1,18 +1,24 @@
 import sys
 
-N = int(sys.stdin.readline())
-coordinates = list(map(int, sys.stdin.readline().split()))
-unique_sorted = sorted(list(set(coordinates)))
-rank_dict = {}
+class Compress_Coordinates:
+    def __init__(self):
+        pass
 
-for i in range(len(unique_sorted)):
-    rank_dict[unique_sorted[i]] = i
+    def dict_sorted(self, target):
+        self.sorted_unique = sorted(set(target))
+        self.target_dict = {num: rank for rank, num in enumerate(self.sorted_unique)}
+        self.compressed_result = []
+        
+        for num in target:
+            self.compressed_result.append(self.target_dict[num])
 
-for num in coordinates:
-    print(rank_dict[num], end=' ')
+        print(*self.compressed_result)    
+
+
+if __name__ == '__main__':
+    my_compress_coordinate = Compress_Coordinates()
+    N = int(sys.stdin.readline())
+    num_line = list(map(int, sys.stdin.readline().split()))
     
-
-
-
-
-
+    my_compress_coordinate.dict_sorted(num_line)
+    
