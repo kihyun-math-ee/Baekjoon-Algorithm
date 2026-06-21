d8a266bf7e71ba6717bf5c19eb17c2efd4567157
Kihyun <kihyun.math.ee@gmail.com>
2026-06-21 11:38:00 +0900

    | 2026-06-21 | Brute Force | 1337 | Implemented a brute-force search over a sorted array, checking how many of the 5 consecutive numbers starting from each element are already present to find the minimum required additions. |

---
+++ BruteForce/1337.py
@@ -0,0 +1,29 @@
+import sys
+
+L = []
+min_add = 5
+N = int(sys.stdin.readline())
+for _ in range(N):
+    n = int(sys.stdin.readline())
+    L.append(n)
+
+L.sort()
+i = 0
+while True:
+    if i < len(L):
+        c = 0
+        if L[i] in L:
+            c += 1
+        if L[i] + 1 in L:
+            c += 1
+        if L[i] + 2 in L:
+            c += 1
+        if L[i] + 3 in L:
+            c += 1
+        if L[i] + 4 in L:
+            c += 1
+        if 5 - c < min_add:
+            min_add = 5 - c
+        i += 1
+    else:
+        break
