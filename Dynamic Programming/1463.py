2dc4a8c0caafa3570efab6080395d8b485a51f5e
Kihyun <kihyun.math.ee@gmail.com>
2026-07-12 00:12:41 +0900

    Add: BOJ 1463 Make it 1 (Implement bottom-up DP to find the minimum operations to reduce a number to 1)

---
+++ Dynamic Programming/1463.py
@@ -0,0 +1,19 @@
+import sys
+
+N = int(sys.stdin.readline())
+dp = [0] * (N + 1)
+dp[1] = 0
+
+for i in range(2, N + 1):
+    if i % 2 == 0 and i % 3 == 0:
+        dp[i] = min(dp[i - 1], dp[i // 2], dp[i // 3]) + 1
+    elif i % 2 == 0:
+        dp[i] = min(dp[i - 1], dp[i // 2]) + 1
+    elif i % 3 == 0:
+        dp[i] = min(dp[i - 1], dp[i // 3]) + 1
+    else:
+        dp[i] = dp[i - 1] + 1
+
+print(dp[N])
+
+
